fb3da0f7d8df62ff9996fd76cd7bdd6778b1e2c4
Alcyxne <149871653+Alcyxne@users.noreply.github.com>
2023-12-01 15:01:08 +0100

    Update b.c

--- jeuSnoopy/b.c
+++ jeuSnoopy/b.c
@@ -4,21 +4,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-void ibo(char matrice[10][20]) {
+#include "snoopy.h"
 
-//  LES OISEAUX
-    matrice[8][1] = 'B';
-    matrice[1][18] = 'B';
-    matrice[1][1] = 'B';
-    matrice[8][18] = 'B';
+void ibo(char matrice[lignes][colonnes]) { //initiatlisation des blocks et oiseaux
+    //  LES OISEAUX
+    matrice[8][1] = oiseau;
+    matrice[1][18] = oiseau;
+    matrice[1][1] = oiseau;
+    matrice[8][18] = oiseau;
 
+    /*matrice[3][11] = bloc;
+    matrice[7][14] = bloc;
+    matrice[8][11] = bloc;
+    matrice[2][18] = bloc;
+    matrice[4][9] = bloc;
+    matrice[3][11] = bloc;
+    matrice[7][14] = bloc;
+    matrice[3][11] = bloc;
+    matrice[7][14] = bloc;*/
+
+    matrice[6][10] = nobreak;
+    matrice[3][7] = nobreak;
+    matrice[8][12] = nobreak;
+    matrice[2][10] = nobreak;
+    matrice[8][17] = nobreak;
+    matrice[5][18] = nobreak;
+    matrice[3][4] = nobreak;
+    matrice[2][15] = nobreak;
 }
 
-void ib(char matrice[10][20]) {   //les blocks cassables
 
-   matrice[4][8]='X';
-   matrice[3][9]='X';
-   matrice[7][14]='X';
-   matrice[2][6]='X';
+void bk(char matrice[lignes][colonnes]) {
 
+
+    // Ajout du bloc incassable
+    int rowH;  // Choisissez la position du bloc incassable
+    int colH;
+    if (rowH >= 0 && rowH < lignes && colH >= 0 && colH < colonnes) {
+        matrice[6][10] = nobreak;
+        matrice[3][7] = nobreak;
+        matrice[8][12] = nobreak;
+        matrice[2][10] = nobreak;
+        matrice[8][17] = nobreak;
+        matrice[5][18] = nobreak;
+        matrice[3][4] = nobreak;
+        matrice[2][15] = nobreak;// 'H' pour le bloc incassable
+    }
 }
