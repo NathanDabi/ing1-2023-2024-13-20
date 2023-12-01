e647a71a5949b0bf8c9c39c26e37d38819979802
Alcyxne <149871653+Alcyxne@users.noreply.github.com>
2023-12-01 15:00:14 +0100

    Update snoopy.h

--- jeuSnoopy/snoopy.h
+++ jeuSnoopy/snoopy.h
@@ -14,14 +14,17 @@ const int lignes = 10;
 const int colonnes = 20;
 
 
-
 int Menu2();
 void im(char matrice[lignes][colonnes]); //initialisation de la matrice
-void dp(char matrice[lignes][colonnes], int* X, int* Y, char direction); //déplacement de Snoopy
+int dp(char matrice[lignes][colonnes], int* X, int* Y, char direction); //déplacement de Snoopy
 void ibo(char matrice[lignes][colonnes]); //initialisation des oiseaux
+void ibo2(char matrice[lignes][colonnes]); //initialisation des oiseaux niveau 2
 void ib(char matrice[lignes][colonnes]); //initialisation des blocks cassables
 void mb(char matrice[lignes][colonnes]); //affichage de la matrice avec les blocks
-
+void bk(char matrice[lignes][colonnes]); //bloc incassable
+void bk2(char matrice[lignes][colonnes]);//bloc incassable niveau2
+void ib2(char matrice[lignes][colonnes]); //initialisation des blocks cassables niveau2
+//int ball(char matrice[lignes][colonnes]);
 
 
 
