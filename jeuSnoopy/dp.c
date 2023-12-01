0bb604bb56ad6d1e8bc6e05c03471a248f193dfc
Alcyxne <149871653+Alcyxne@users.noreply.github.com>
2023-12-01 15:01:41 +0100

    Update dp.c

--- jeuSnoopy/dp.c
+++ jeuSnoopy/dp.c
@@ -1,29 +1,64 @@
-//
-// Created by sacha on 14/11/2023.
-//
 #include "snoopy.h"
+#include <stdio.h>
 
-void dp(char matrice[10][20], int* X, int* Y, char direction) { //deplacement de snoopy
-    matrice[*X][*Y] = ' ';
+
+int dp(char matrice[lignes][colonnes], int* X, int* Y, char direction) {
+    int retour = rien;
+
+    matrice[*X][*Y] = case_vide;
+
+    int newX = *X;
+    int newY = *Y;
 
     switch (direction) {
-        case 'h':
-            if (*X > 1) (*X)--;
+        case 'z':
+            newX = *X - 1;
             break;
-        case 'b':
-            if (*X < 8) (*X)++;
+        case 's':
+            newX = *X + 1;
             break;
-        case 'g':
-            if (*Y > 1) (*Y)--;
+        case 'q':
+            newY = *Y - 1;
             break;
         case 'd':
-            if (*Y < 18) (*Y)++;
-
+            newY = *Y + 1;
             break;
     }
-    matrice[*X][*Y] = 'S';
 
+    // Vérification de la validité de la nouvelle position
+    if (newX >= 0 && newX < lignes && newY >= 0 && newY < colonnes) {
+        char newCell = matrice[newX][newY];
+
+        if (newCell != mur && newCell != nobreak) {
+            if (newCell == oiseau) {
+                // Gérer ici les points
+                retour = point;
+
+            }else if (newCell == bloc) {
+                // Perdre une vie si on rencontre un bloc
+
+
+                retour = vie_en_moins;
+            }
+
+            // Mettre à jour les nouvelles coordonnées
+            *X = newX;
+            *Y = newY;
+
+            matrice[*X][*Y] = snoopy;
+
+            printf("Deplacement reussi. Nouvelles coordonnees : X=%d, Y=%d\n", *X, *Y);
+        } else {
+            printf("Deplacement bloque. Nouvelles coordonnees : X=%d, Y=%d, Valeur de la cellule : %c\n", newX, newY, newCell);
+            matrice[*X][*Y] = snoopy;
+        }
+    }
+
+    return retour;
 }
+
+
+
 
 
 
