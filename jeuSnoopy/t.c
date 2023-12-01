a98bd2f103f567fce7248afdf5c5b47f7b68dc65
Alcyxne <149871653+Alcyxne@users.noreply.github.com>
2023-12-01 15:05:44 +0100

    Update t.c

--- jeuSnoopy/t.c
+++ jeuSnoopy/t.c
@@ -2,10 +2,10 @@
 // Created by sacha on 14/11/2023.
 //
 #include <stdio.h>
-
+#include <unistd.h>
 #include <time.h>
 
-void Te(int* t) { // tentative du chrono
+/*void Te(int* t) { // tentative du chrono
     time_t debut = time(NULL);
     time_t maintenant;
     int duree = 120;  // 120 secondes
@@ -19,6 +19,23 @@ void Te(int* t) { // tentative du chrono
             break;
         }
     }
+}
+*/
+int Temps() {
+int t = 120;
+    while (t > 0) {
+printf("           Temps = %d", t);
+fflush(stdout);
+sleep(1);
+
+printf("\r");
+for (int i = 0; i < 10; i++) {
+printf(" ");
+}
+printf("\r");
+t--;
+}
+printf("Temps = 0\n");
 }
 
 
