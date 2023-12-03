4a481180d2d674f7d6201051a669ad18dc4a8877
Alcyxne <149871653+Alcyxne@users.noreply.github.com>
2023-12-03 13:04:30 +0100

    Update menu5.c

--- jeuSnoopy/menu5.c
+++ jeuSnoopy/menu5.c
@@ -1,12 +1,12 @@
 //
-// Created by mathi on 20/11/2023.
+// Created by mathi on 03/12/2023.
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int Menu5() //quitter le programme
 {
+    sleep(3);
     exit(0); // ferme automatiquement la page
 }
-
-//voir si ça ferme pas automatiquement les autres menus, normalement ça doit pas être le cas mais bon
